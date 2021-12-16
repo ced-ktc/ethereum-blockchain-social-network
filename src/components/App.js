1d944cfc79255ad2751696f8a8a7e8fba9ce8517
BrouceM <cedrick.kamsi@gmail.com>
2021-12-16 19:46:04 +0100

    feat: fetching the list of posts

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, useLayoutEffect } from 'react';
 import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
+import SocialNetwork from '../abis/SocialNetwork.json';
 import Navbar from './Navbar';
 
 class App extends Component {
@@ -39,13 +40,40 @@ class App extends Component {
 
     const accounts = await web3.eth.getAccounts();
     this.setState({account: accounts[0]})
+
+    //Network ID
+    const networkId = await web3.eth.net.getId();
+    console.log("networkId", networkId);
+    const networkData = SocialNetwork.networks[networkId]
+    if(networkData){
+      //fetch the smart contract
+      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
+      this.setState({socialNetwork})
+      const postCount = await socialNetwork.methods.postCount().call()
+      this.setState({postCount})
+      //Load posts
+      for(var i=1; i<=postCount; i++){
+        const post = await socialNetwork.methods.posts(i).call()
+        this.setState({
+          posts:[...this.state.posts, post]
+        })
+      }
+      console.log({posts:this.state.posts})
+    }else{
+      window.alert('SocialNetwork contract not deployed to detected network')
+    }
+    //Address
+    //ABI
   }
 
 
   constructor(props){
     super(props)
     this.state = {
-      account:''
+      account:'',
+      socialNetwork:null,
+      postCount:0,
+      posts:[]
     }
   }
 
@@ -55,27 +83,25 @@ class App extends Component {
         <Navbar account = {this.state.account}/>
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{maxWidth :'500px'}}>
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+               {this.state.posts.map((post, key)=>{
+                 return(
+                   <div className="card mb-4" key={key}>
+                    <div className="card_header">
+                      <small className="text-muted">{post.author}r</small>
+                    </div>
+                    <ul id="postList" className="list-group list-group-flush">
+                      <li className="list-group-item">
+                        <p>{post.content}</p>
+                      </li>
+                      <li key={key} className="list-group-item py-2">
+                        <p>Post Footer</p>
+                      </li>
+                    </ul>
+                   </div>
+                 )
+               })}
               </div>
             </main>
           </div>
