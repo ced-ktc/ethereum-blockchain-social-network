41e7062d2c7082425c6c35c932804693c9b9b8be
BrouceM <cedrick.kamsi@gmail.com>
2021-12-22 13:16:40 +0100

    feat: tip a post and sort the list by the tipAmounts belong to each post

--- src/components/App.js
+++ src/components/App.js
@@ -59,6 +59,9 @@ class App extends Component {
           posts:[...this.state.posts, post]
         })
       }
+      this.setState({
+        posts: this.state.posts.sort((a,b)=>b.tipAmount - a.tipAmount)
+      })
       this.setState({loading:false})
     }else{
       window.alert('SocialNetwork contract not deployed to detected network')
@@ -67,11 +70,17 @@ class App extends Component {
     //ABI
   }
 
-  createPost(content){
+  async createPost(content){
     this.setState({ loading:true })
-    this.state.socialNetwork.methods.createPost(content).send({ from:this.state.account })
-    .once('receipt', (receipt) => {
-      this.setState({ loading:false })
+    this.state.socialNetwork.methods.createPost(content).send({ from:this.state.account }).once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
+  }
+
+  async tipPost(id, tipAmount){
+    this.setState({loading: true})
+    this.state.socialNetwork.methods.tipPost(id).send({ from: this.state.account, value: tipAmount }).once('receipt', (receipt)=>{
+      this.setState({loading:false})
     })
   }
 
@@ -83,9 +92,10 @@ class App extends Component {
       socialNetwork:null,
       postCount:0,
       posts:[],
-      loading:true
+      loading: true
     }
     this.createPost = this.createPost.bind(this)
+    this.tipPost = this.tipPost.bind(this)
   }
 
   render() {
@@ -97,6 +107,7 @@ class App extends Component {
           :<Main
             posts = {this.state.posts}
             createPost = {this.createPost}
+            tipPost = {this.tipPost}
           />
         }        
       </div>
